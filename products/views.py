bcd3b93b5da829c7b30d427303883d30525330cb
DaveTrev <trevaskisdavid@gmail.com>
2023-10-30 17:52:29 +0000

    corrected typo in product views

--- products/views.py
+++ products/views.py
@@ -19,6 +19,6 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': product, }
+        'product': product}
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/product_detail.html', context)
