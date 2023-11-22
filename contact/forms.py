8c61b5791a855f18f2ed576a559aaae2a3d1f9be
DaveTrev <trevaskisdavid@gmail.com>
2023-11-22 21:43:47 +0000

    added contact reason to form and model of contact app

--- contact/forms.py
+++ contact/forms.py
@@ -6,7 +6,7 @@ from crispy_forms.layout import Submit, Layout, Fieldset, ButtonHolder
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ['name', 'email', 'message']
+        fields = ['contact_reason', 'name', 'email', 'message',]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
+CONTACT_REASONS = (
+    ("general_query", "General Query"),
+    ("glasses", "Glasses"),
+    ("sunglasses", "Sunglasses"),
+    ("technical_issue", "Technical Issue"),
+    ("other", "Other"),
+)
+
 class Contact(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.EmailField()
-    message = models.TextField(max_length=400)
+    
+    contact_reason = models.CharField(
+        max_length=100,
+        choices=CONTACT_REASONS,
+        default="general_query",
+        null=False,
+        blank=False,
+    )
+    name = models.CharField(null=False, blank=False, max_length=50,)
+    email = models.EmailField(null=False, blank=False, max_length=250,)
+    message = models.TextField(max_length=400, null=False, blank=False,)
 
     def __str__(self):
         return f"{self.name} - {self.email}"
