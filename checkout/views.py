68143bd9045a2eeb25834de7ab1808b10738b75e
DaveTrev <trevaskisdavid@gmail.com>
2023-12-04 10:14:48 +0000

    added comments to checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -18,6 +18,9 @@ import json
 
 @require_POST
 def cache_checkout_data(request):
+    """
+    Cache checkout data
+    """
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -34,6 +37,9 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
+    """
+    Checkout functionality
+    """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
